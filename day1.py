53c29cdb3dc7aad84be0551adf8919144b415e00
Jessica Tiberio <32365549+JTibs18@users.noreply.github.com>
2020-12-04 14:19:24 -0500

    Improved speed of algorithm

--- day1.py
+++ day1.py
@@ -1,29 +1,44 @@
 #Part 1 Solution
 f1 = open("day1Input.txt") 
 nums = [] 
+num1 = 0
+num2 = 0
 
 for line in f1: 
 
         line = line.rstrip() 
         nums.append(line)
 
-for i, num in enumerate(nums): 
-        for j, num2 in enumerate(nums): 
-                if ((int(num) + int(num2)) == int(2020)): 
-                        print(int(num) * int(num2))
+for n in nums: 
+        for n2 in nums: 
+                if ((int(n) + int(n2)) == int(2020)): 
+                        num1 = int(n) 
+                        num2 = int(n2)
+                        break
+
+print(num1 * num2)
+
 
 
 #Part 2 Solution 
 f1 = open("day1Input.txt") 
 nums = [] 
+num1 = 0
+num2 = 0
+num3 = 0
 
 for line in f1: 
 
         line = line.rstrip() 
         nums.append(line)
 
-for i, num in enumerate(nums): 
-        for j, num2 in enumerate(nums): 
-                for k, num3 in enumerate(nums): 
-                        if ((int(num) + int(num2) + int(num3)) == int(2020)): 
-                                print(int(num) * int(num2)* int(num3))
+for n in nums: 
+        for n2 in nums: 
+                for n3 in nums: 
+                        if ((int(n) + int(n2) + int(n3)) == int(2020)): 
+                                num1 = n
+                                num2 = n2 
+                                num3 = n3
+                                break 
+
+print(int(num1) * int(num2)* int(num3))
